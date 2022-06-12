866776d39de03bf3f79909dfa9614382eb7e0de1
Konstantin Zaytsev <tarans.fishborn@gmail.com>
2022-06-13 02:32:09 +0300

    Домашняя работа №7, 4 задача

---
+++ dz_7_task_4.py
@@ -0,0 +1,15 @@
+import os
+my_files = []
+for r, d, f in os.walk('./'):
+    for file in f:
+        file_path = os.path.join(r, file)
+        my_files.append(os.stat(file_path).st_size)
+max_size = max(my_files)
+i = 1
+out_dict = {}
+for one in range(len(str(max_size))):
+    i *= 10
+    out_dict[i] = 0
+for file in my_files:
+    out_dict[10 ** len(str(file))] += 1
+print(out_dict)
